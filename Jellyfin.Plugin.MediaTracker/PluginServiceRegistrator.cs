60b6b360d33330bfeabbdde1a03f2e505c33d8c7
bonukai <17750886+bonukai@users.noreply.github.com>
2024-05-16 20:14:22 +0100

    Upgrade to .net 8

---
+++ Jellyfin.Plugin.MediaTracker/PluginServiceRegistrator.cs
@@ -0,0 +1,17 @@
+﻿using MediaBrowser.Controller;
+using MediaBrowser.Controller.Plugins;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jellyfin.Plugin.MediaTracker;
+
+/// <summary>
+/// Register plugin service.
+/// </summary>
+public class PluginServiceRegistrator : IPluginServiceRegistrator
+{
+    /// <inheritdoc />
+    public void RegisterServices(IServiceCollection serviceCollection, IServerApplicationHost applicationHost)
+    {
+        serviceCollection.AddHostedService<ServerEntryPoint>();
+    }
+}
